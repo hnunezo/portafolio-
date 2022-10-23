5d8a2a9936ee7675fcc84297ef541b9e067be4b2
hnunezo <hector.nunez.oviedo@gmail.com>
2022-10-23 12:24:54 -0300

    adding-projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -13,7 +13,7 @@ const Projects = ({ projectsRef }) => {
         <h2 className="align-self-center">PROJECTS</h2>
         <div className="d-flex flex-wrap justify-content-center">
           <FlipCard
-            title={"Anime Searcher"}
+            title={"WeebList"}
             description={
               "A complete browser for anime series, manga, characters and voice actors. from the anime industry, you can bookmark it and order it for rank in a top."
             }
@@ -36,36 +36,31 @@ const Projects = ({ projectsRef }) => {
             img={"mathgram"}
             href="https://mathgram.vercel.app/"
             tasks={[
-              "Solve math exercises for practice.",
-              "Evaluate yourself with percentages to pass each exam.",
+              "Solve math exercises for practice",
+              "Evaluate yourself with percentages to pass each exam",
             ]}
           />
           <FlipCard
             title={"Pokemon App"}
-            description={"ASDASD"}
-            img={"weeblist"}
-            href="https://weeb-list.vercel.app/"
+            description={"Be the trainer you always wanted to be!"}
+            img={"poke-app"}
+            href="https://poke-app-eight-pi.vercel.app/"
             tasks={[
-              "Anime series searching",
-              "Manga series searching",
-              "Japanese characters searching",
-              "Japanese voice actors searching",
-              "Favorites list",
-              "Draggable favorites order",
+              "Catch wilds pokemon",
+              "Form your team",
+              "Fill the pokedex",
+              "Save your pokemons on PC",
             ]}
           />
           <FlipCard
             title={"NASA App"}
-            description={"ASDASD"}
-            img={"weeblist"}
-            href="https://weeb-list.vercel.app/"
+            description={"Discover the universe in a fun way!"}
+            img={"nasaapp"}
+            href="https://nasa-app-iota.vercel.app/"
             tasks={[
-              "Anime series searching",
-              "Manga series searching",
-              "Japanese characters searching",
-              "Japanese voice actors searching",
-              "Favorites list",
-              "Draggable favorites order",
+              "Discover incredible images of the universe, with the random function",
+              "Daily image of the universe with the story behind",
+              "Perform custom searches to deliver related images and find out how they were taken",
             ]}
           />
         </div>
